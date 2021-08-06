fed4f74432635ed7716c37c0323942b6d6378cd7
Marco Randazzo <marco.randazzo@iit.it>
2021-08-06 18:07:21 +0200

    added option --yarplogname to set a name for the executable in the LogForwarder

--- src/libYARP_os/src/yarp/os/impl/LogForwarder.cpp
+++ src/libYARP_os/src/yarp/os/impl/LogForwarder.cpp
@@ -31,9 +31,26 @@ yarp::os::impl::LogForwarder::LogForwarder()
     yarp::os::gethostname(hostname, HOST_NAME_MAX);
 
     yarp::os::SystemInfo::ProcessInfo processInfo = yarp::os::SystemInfo::getProcessInfo();
+    std::string args = processInfo.arguments;
+    std::string yarp_proc_name = "null";
+
+    const std::string  key = "--yarplogname ";
+    size_t beg = args.find (key);
+    if (beg != std::string::npos)
+    {
+        size_t beg2= beg + key.size();
+        size_t end = args.find(" ", beg2);
+        {
+            yarp_proc_name = args.substr(beg2, end - beg2);
+            if (yarp_proc_name.find("--") != std::string::npos)
+            {
+                yarp_proc_name = "error";
+            }
+        }
+    }
 
     outputPort.setWriteOnly();
-    std::string logPortName = "/log/" + std::string(hostname) + "/" + processInfo.name.substr(processInfo.name.find_last_of("\\/") + 1) + "/" + std::to_string(processInfo.pid);
+    std::string logPortName = "/log/" + std::string(hostname) + "/" + processInfo.name.substr(processInfo.name.find_last_of("\\/") + 1) + "/" + std::to_string(processInfo.pid) + "/" + yarp_proc_name;
     if (!outputPort.open(logPortName)) {
         printf("LogForwarder error while opening port %s\n", logPortName.c_str());
     }
